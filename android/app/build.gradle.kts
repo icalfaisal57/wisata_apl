f79b0895f7f090e5c93a97c2a25a5461e0f74cc1
icalfaisal57 <icalfaisal57@gmail.com>
2025-06-18 10:21:58 +0700

    3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,16 +8,14 @@ plugins {
 android {
     namespace = "com.example.wisata_apl"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+   
+    ndkVersion = "27.0.12077973" 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
